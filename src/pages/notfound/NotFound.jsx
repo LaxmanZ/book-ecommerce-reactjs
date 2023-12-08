760f9fdf8f0bca414af32a69dc3ebad6db269483
LaxmanZ <bhajantrilaxman2001@gmail.com>
2023-12-08 13:05:42 +0530

    designed the notfound page

--- src/pages/notfound/NotFound.jsx
+++ src/pages/notfound/NotFound.jsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
+import './notfound.css';
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <section className="notFound">
+      <div className="container notfound-container">
+        <h1>404</h1>
+        <h3>Opps, This Page Not Found</h3>
+        <Link to={'/'} className="btn btn-border">
+          Go To Home
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default NotFound;
